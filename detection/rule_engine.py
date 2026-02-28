3f3599c566f09c394d6de4c3cd2490c80e857ede
Vedant Borade <146107374+Vedantfr@users.noreply.github.com>
2026-02-28 21:19:23 +0530

    Add files via upload

---
+++ detection/rule_engine.py
@@ -0,0 +1,49 @@
+<<<<<<< HEAD
+def analyze(data):
+    threats = []
+
+    system = data.get("system", {})
+    if system.get("cpu", 0) > 90:
+        threats.append({"type": "high_cpu"})
+
+    processes = data.get("processes", [])
+    for proc in processes:
+        threats.append({
+            "type": "suspicious_process",
+            "pid": proc.get("pid")
+        })
+
+    network = data.get("network", [])
+    for conn in network:
+        threats.append({
+            "type": "suspicious_connection",
+            "ip": conn.get("ip")
+        })
+
+    return threats
+
+=======
+def analyze(data):
+    threats = []
+
+    system = data.get("system", {})
+    if system.get("cpu", 0) > 90:
+        threats.append({"type": "high_cpu"})
+
+    processes = data.get("processes", [])
+    for proc in processes:
+        threats.append({
+            "type": "suspicious_process",
+            "pid": proc.get("pid")
+        })
+
+    network = data.get("network", [])
+    for conn in network:
+        threats.append({
+            "type": "suspicious_connection",
+            "ip": conn.get("ip")
+        })
+
+    return threats
+
+>>>>>>> ff48c825f9fd64ae919885467895d38972d81c36
