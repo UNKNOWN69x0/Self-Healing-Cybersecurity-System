a4d8de4423d11f6c7a75e7739df6b0cd8262acf3
Vedant Borade <146107374+Vedantfr@users.noreply.github.com>
2026-02-28 21:20:19 +0530

    Add files via upload

---
+++ monitor/system_monitor.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+import psutil
+
+def get_system_metrics():
+    return {
+        "cpu": psutil.cpu_percent(interval=1),
+        "memory": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage('/').percent
+    }
+=======
+import psutil
+
+def get_system_metrics():
+    return {
+        "cpu": psutil.cpu_percent(interval=1),
+        "memory": psutil.virtual_memory().percent,
+        "disk": psutil.disk_usage('/').percent
+    }
+>>>>>>> ff48c825f9fd64ae919885467895d38972d81c36
